13c02324ccf90ed2e22bc5c816da6f02efe50abd
Anton Sadovnikov <anton.sadovnikov@rambler-co.ru>
2018-01-11 16:53:23 +0300

    fix switch

--- health_hook.go
+++ health_hook.go
@@ -9,26 +9,22 @@ type HealthHook struct {
 }
 
 func NewHealthHook(version string) *HealthHook {
-	return &HealthHook{New("1.0.0")}
+	return &HealthHook{New(version)}
 }
 
 func (hook *HealthHook) Fire(entry *logrus.Entry) error {
 	switch entry.Level {
 	case logrus.PanicLevel:
 		hook.Health.incError()
-		return nil
 	case logrus.FatalLevel:
 		hook.Health.incError()
-		return nil
 	case logrus.ErrorLevel:
 		hook.Health.incError()
-		return nil
 	case logrus.WarnLevel:
 		hook.Health.incWarning()
-		return nil
-	default:
-		return nil
 	}
+
+	return nil
 }
 
 func (hook *HealthHook) Levels() []logrus.Level {
